16e003ffc46825faeac1c541ab92fff86dcdc141
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-05-10 09:33:47 -0500

    add my favorite

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -14,7 +14,8 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-    postFavorite: campsiteId => (postFavorite(campsiteId))
+    postFavorite: campsiteId => (postFavorite(campsiteId)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text))
 };
 
 
@@ -24,22 +25,32 @@ function RenderComments({comments}) {
         return (
             <View style={{margin: 10}}>
                 <Text style={{fontSize: 14}}>{item.text}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
                 <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+                <Rating
+                    readonly
+                    imageSize={10}
+                    style={{alignItems:'flex-start', paddingVertical:'5%'}}
+                    startingValue={item.rating}
+                >
+
+                </Rating>
             </View>
         );
     };
 
-    return (
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
-    );
+    if (comments) {
+        return (
+            <Card title='Comments'>
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        );
+    }
 }
+
 
 function RenderCampsite(props) {
 
@@ -62,10 +73,10 @@ function RenderCampsite(props) {
                         raised
                         reverse
                         onPress={() => props.favorite ? 
-                            console.log('Already set as a favorite') : props.markFavorite()}
+                        console.log('Already set as a favorite') : props.markFavorite()}
                     />
 
-                        <Icon 
+                    <Icon 
                         name='pencil'
                         type='font-awesome'
                         color='#5637DD'
@@ -110,6 +121,12 @@ class CampsiteInfo extends Component {
         });
     }
 
+    handleComment(campsiteId){
+        this.props.postComment(campsiteId, this.state.rating, this.state.author, this.state.text);
+        this.toggleModal();
+    }
+
+
     static navigationOptions = {
         title: 'Campsite Information'
     }
@@ -133,16 +150,53 @@ class CampsiteInfo extends Component {
                     visible={this.state.showModal}
                     onRequestClose={() => this.toggleModal()}>
 
-                    <View style={{margin:10}}>
-                        <Button
-                            onPress={() => {
-                                this.toggleModal();   //not showing modal
-                                this.resetForm();
-                            }}
-                            color='#808080'
-                            title='Cancle'
-                            
+                        <View style={styles.modal}>
+                            <Rating
+                                showRating
+                                startingValue={this.state.rating}
+                                imageSize={40}
+                                onFinishRating={(rating)=>this.setState({rating: rating})} 
+                                style={{ paddingVertical: 10 }}
+                                />
+                        <Input 
+                            placeholder='Author'
+                            leftIcon = {{type: 'font-awesome', name: 'user-o'}}
+                            leftIconContainerStyle = {{paddingRight: 10}}
+                            onChangeText = {author => this.setState({author: author})}
+                            value = {this.state.author}
                         />
+
+                        <Input 
+                            placeholder='Comment'
+                            leftIcon = {{type: 'font-awesome', name: 'comment-o'}}
+                            leftIconContainerStyle = {{paddingRight: 10}}
+                            onChangeText = {text => this.setState({text: text})}
+                            value = {this.state.text}
+                        />
+                        
+                        <View style={{margin:10}}>
+                            <Button
+                                onPress={() => {
+                                    this.handleComment(campsiteId)
+                                    this.resetForm();     // close modal > showmodal value is false
+                                }}
+                                color='#5637DD'
+                                title='Submit'
+                                
+                            />
+                        </View>
+
+                        <View style={{margin:10}}>
+                            <Button
+                                onPress={() => {
+                                    this.toggleModal();   //showing modal
+                                    this.resetForm();     // close modal > showmodal value is false
+                                }}
+                                color='#808080'
+                                title='Cancle'
+                                
+                            />
+                        </View>
                     </View>
                 </Modal>
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
@@ -70,6 +71,29 @@ const DirectoryNavigator = createStackNavigator(
                 color: '#fff'
             }
         }
+    }
+);
+
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
     }
 );
 
@@ -216,6 +240,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='info-circle'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
