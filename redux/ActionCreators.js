0f02319187402d055be5cc61c83afcd47c28361c
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-05-10 09:54:41 -0500

    add delete section

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -172,3 +172,7 @@ export const addFavorite = campsiteId => ({
     payload: campsiteId
 });
 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
--- redux/ActionTypes.js
+++ redux/ActionTypes.js
@@ -15,5 +15,6 @@ export const ADD_PARTNERS = 'ADD_PARTNERS';
 export const PARTNERS_FAILED = 'PARTNERS_FAILED';
 
 export const ADD_FAVORITE = 'ADD_FAVORITE';
+export const DELETE_FAVORITE = 'DELETE_FAVORITE'; 
 
 
